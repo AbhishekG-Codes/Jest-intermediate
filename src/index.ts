ba2eeac431ca55d32875cd0b4c95a019c5d892dd
AbhishekG-Codes <166795493+AbhishekG-Codes@users.noreply.github.com>
2025-03-17 15:27:11 +0530

    checkcomments-index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,17 @@ app.patch("/update-age", (req: Request, res: Response) => {
     const { age } = req.body;
     res.json({ message: `Updated age to ${age}` });
 });
+
+// import express, { Request, Response } from "express";
+
+// export const app = express();
+// app.use(express.json());
+
+// app.post("/add", (req: Request, res: Response) => {
+//     const { num1, num2 } = req.body;
+//     if (typeof num1 !== "number" || typeof num2 !== "number") {
+//         return res.status(400).json({ error: "Both num1 and num2 must be numbers" });
+//     }
+//     const sum = num1 + num2;
+//     res.json({ sum });
+// });
